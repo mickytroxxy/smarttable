21f100b078f9d73a0c41b32f840230d5057f0d38
mickytroxxy <mickylyricalkartel@gmail.com>
2019-11-30 11:06:37 +0200

    Create health.js

---
+++ health.js
@@ -0,0 +1,19 @@
+base url is http://techapis.xyz:4400/
+
+Register a new patient with 
+
+baseUrl/register/patientName/patientSurname/patientSurname/patientBdae/patientBdae/patientAddress/patientAddress/patientEmail/patientEmail/patientPhone/patientPhone/patientGender/patientGender/patientPassword/patientPassword/addedBy/addedBy
+
+The above request returns the id as the file number on success, returns 0 on failure
+
+Get patient details 
+
+baseUrl/getMyDetails/fileNo
+
+The above url returns json data
+
+Get patient medical Records
+
+baseUrl/getMedicalHistory/fileNo
+
+The above url returns json data

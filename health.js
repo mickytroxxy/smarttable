f03761b516577d06646ee45feb79bc37f7b5a540
mickytroxxy <mickylyricalkartel@gmail.com>
2019-12-12 11:32:19 +0200

    Update health.js

--- health.js
+++ health.js
@@ -2,7 +2,7 @@ base url is http://techapis.xyz:4400/
 
 Register a new patient with 
 
-baseUrl/register/patientName/patientSurname/patientSurname/patientBdae/patientBdae/patientAddress/patientAddress/patientEmail/patientEmail/patientPhone/patientPhone/patientGender/patientGender/patientPassword/patientPassword/addedBy/addedBy
+baseUrl/register/patientName/patientSurname/patientSurname/patientBdae/patientBdae/patientAddress/patientAddress/patientEmail/patientEmail/patientPhone/patientPhone/patientGender/patientGender/patientPassword/patientPassword/addedBy/addedBy/nextOfKin1/nextOfKin1/nextOfKin2/nextOfKin2
 
 The above request returns the id as the file number on success, returns 0 on failure
 
@@ -17,3 +17,15 @@ Get patient medical Records
 baseUrl/getMedicalHistory/fileNo
 
 The above url returns json data
+
+//GET all doctors
+baseUrl/getAllPractitioners
+//returns json data
+
+//get practitioners
+baseUrl/getPractitioners/specialization/latitude/latitude/longitude/longitude
+//returns json data
+
+//setup appointment
+baseUrl/setAppointment/fileNo/patientName/patientName/contact/contact/reason/reason/preferredTime/preferredTime/practitionerId/practitionerId
+//returns bolean, true for success, false for error

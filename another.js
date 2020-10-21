43efbac36856d8ba23c2eb2ee7d81abc11db0d0b
mickytroxxy <mickylyricalkartel@gmail.com>
2020-10-21 14:03:15 +0200

    Create another.js

---
+++ another.js
@@ -0,0 +1,30 @@
+//$$(".scrolling-wrapper").append("<div class='card shop-list-ul-card go-to-shop-panel-btn' shopId='"+shopId+"' style='width: 130px;border-radius:10px;box-shadow:none;border: 1px solid #e9eaed;'><div class='card-header' style='text-transform: uppercase;color: #757575;font-size: 10px;font-weight: bold;min-height:40px;white-space: nowrap; overflow: hidden;text-overflow: ellipsis;padding-right:3px;'>"+shopName+"</div><div class='card-content'><div class='card-content-inner'><center><i class='material-icons color-orange' style='font-size:50px;'>shopping_cart</i></center></div></div><div class='card-footer' style='font-weight:bold; font-size:10px;min-height:30px;'>"+category.toUpperCase()+"</div></div>");
+
+
+
+<div class="row no-gutter">
+						<div class="col-33 looking-for-page-btn">
+							<div class="card" style="border-radius:5px;box-shadow:none;">
+								<div class="card-content">
+									<div class="card-content-inner"><center><i class="material-icons color-indigo" style="font-size:50px;">directions</i></center></div>
+								</div>
+								<div class="card-footer" style="font-size:9px;color:#757575;font-weight:bold;min-height:25px;">ITEM SEARCH</div>
+							</div>
+						</div>
+						<div class="col-33 driver-account-btn">
+							<div class="card" style="border-radius:5px;box-shadow:none;">
+								<div class="card-content">
+									<div class="card-content-inner"><center><i class="material-icons color-teal" style="font-size:50px;">local_taxi</i></center></div>
+								</div>
+								<div class="card-footer" style="font-size:9px;color:#757575;font-weight:bold;min-height:25px;">DRIVER ACC</div>
+							</div>
+						</div>
+						<div class="col-33 pawn-btn">
+							<div class="card" style="border-radius:5px;box-shadow:none;">
+								<div class="card-content">
+									<div class="card-content-inner"><center><i class="material-icons color-pink" style="font-size:50px;">loyalty</i></center></div>
+								</div>
+								<div class="card-footer" style="font-size:9px;color:#757575;font-weight:bold;min-height:25px;">PAWN & SELL</div>
+							</div>
+						</div>
+					</div>
